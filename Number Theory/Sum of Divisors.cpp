2a438098b56b5ab7e0a2e30c67387072a46f3f82
survival_001 <53923101+ujjawalrana@users.noreply.github.com>
2021-09-17 13:08:31 +0530

    Update Sum of Divisors.cpp

--- Number Theory/Sum of Divisors.cpp
+++ Number Theory/Sum of Divisors.cpp
@@ -1,8 +1,10 @@
 // This will give the sum of divisors of a number
 
-vector<ll>sum(n+1,0);
-for(ll i=1;i<=n;i++){
- 	for(ll j=i;j<=n;j+=i){
-  		sum[j]+=i;
- 	}
+void help(ll n){
+    vector<ll>sum(n+1,0);
+    for(ll i=1;i<=n;i++){
+        for(ll j=i;j<=n;j+=i){
+            sum[j]+=i;
+        }
+    }
 }
