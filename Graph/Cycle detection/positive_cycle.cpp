c1ba57b4c51f2ad54153db2431a1df6df70cf682
survival_001 <53923101+ujjawalrana@users.noreply.github.com>
2021-09-23 09:57:44 +0530

    Create positive_cycle.cpp
    cycle detection using dfs

---
+++ Graph/Cycle detection/positive_cycle.cpp
@@ -0,0 +1,28 @@
+// Time complexity O(M) 
+
+vector<ll>vis(1e5+10,0);
+bool dfs(ll v) {
+    vis[v]=1;
+    for (ll u:a[v]){
+        if (vis[u]==0){
+            // parent[u] = v; for finding the cycle
+            if(dfs(u))
+                return true;
+        } 
+        else if(vis[u]==1){
+            // cycle_end = v , cycle_start = u;
+            return true;
+        }
+    }
+    vis[v]=2;
+    return false;
+}
+
+void solve(){
+    for(int i=1;i<=n;i++){
+        if(vis[i]==0 and dfs(i)){
+            // cycle detected
+            ok=1; break;
+        }
+    }
+}
