065bf9683a384043d9a55fbe9cb713cf3a2c9fea
Ujjawal Rana <53923101+ujjawalrana@users.noreply.github.com>
2021-09-24 11:55:40 +0530

    Create Bipartite test using dfst.cpp

---
+++ Graph/Bipartite test using dfst.cpp
@@ -0,0 +1,21 @@
+// Each vertex belongs to exactly one of the 2 color(0/1)
+// Each edge connects vertices of 2 different sets
+// If the color of parent and child are same then return false else return true
+
+void dfs(ll pos,ll ok){
+	vis[pos]=1;
+	col[pos]=ok;
+
+	for(auto i:a[pos]){
+		if(!vis[i]){
+			if(dfs(i,1-ok))
+				return true;
+			else return false;
+		}
+		else if(col[pos]==col[i])
+			return false;
+	}
+
+	return true;
+}
+
