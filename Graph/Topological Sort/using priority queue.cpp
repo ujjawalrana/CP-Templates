a65a9733ab681b8612cfe6fcb090b51c8b35acfa
Ujjawal Rana <53923101+ujjawalrana@users.noreply.github.com>
2021-09-24 11:21:38 +0530

    Update using priority queue.cpp

--- Graph/Topological Sort/using priority queue.cpp
+++ Graph/Topological Sort/using priority queue.cpp
@@ -1,9 +1,9 @@
+// 'a' is graph and 'n' is no. of nodes
+// 'in' representes the indegree of a node
+// 'topo' contains the sorted topological order
+// using minimum priority queue so that we can get the lexicographically smallest order
+
 bool topological_sort(vector<vector<ll>>&a,vector<ll>&in,ll n){
-  
-    // 'a' is graph and 'n' is no. of nodes
-    // 'in' representes the indegree of a node
-    // 'topo' contains the sorted topological order
-    // using minimum priority queue so that we can get the lexicographically smallest order
   
     priority_queue<ll,vector<ll>,greater<ll>>q;
     for(ll i=1;i<=n;i++){
