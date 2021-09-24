d78330a652619eec75957c6656925afa78b2844f
Ujjawal Rana <53923101+ujjawalrana@users.noreply.github.com>
2021-09-24 11:21:00 +0530

    Update and rename code2.cpp to using dfs.cpp

--- Graph/Topological Sort/using dfs.cpp
+++ Graph/Topological Sort/using dfs.cpp
@@ -1,7 +1,8 @@
+// 'a' is graph and 'pos' is a node
+// don't forget to reverse the 'topo' vector
+
 void topo_dfs(vector<vector<ll>>&a,vector<bool>&vis,vector<ll>&topo,ll pos){
   
-    // 'a' is graph and 'pos' is a node
-    // don't forget to reverse the 'topo' vector
     vis[pos]=true;
     for(auto i:a[pos]){
         if(!vis[i]){
