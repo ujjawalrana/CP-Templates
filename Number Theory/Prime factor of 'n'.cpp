9ccb068e122fb49955fe5f29946f4d6587b08017
Ujjawal Rana <53923101+ujjawalrana@users.noreply.github.com>
2021-09-27 13:30:15 +0530

    Update Prime factor of 'n'.cpp

--- Number Theory/Prime factor of 'n'.cpp
+++ Number Theory/Prime factor of 'n'.cpp
@@ -20,3 +20,31 @@ void prime_factor(ll n){
 
     if(n>1)prime.pb(n);
 }
+
+
+// copied from cp algorithm
+// this leaves us with only 33.3% percent of the numbers to check
+// Testing phase is pending
+
+vector<long long> trial_division3(long long n) {
+    vector<long long> factorization;
+    for (int d : {2, 3, 5}) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+    }
+    static array<int, 8> increments = {4, 2, 4, 2, 4, 6, 2, 6};
+    int i = 0;
+    for (long long d = 7; d * d <= n; d += increments[i++]) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+        if (i == 8)
+            i = 0;
+    }
+    if (n > 1)
+        factorization.push_back(n);
+    return factorization;
+}
