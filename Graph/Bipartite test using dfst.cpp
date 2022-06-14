e0709593a8ee73ce0dcf3778cfb547b68cb1750a
Ujjawal Rana <53923101+ujjawalrana@users.noreply.github.com>
2022-06-14 12:22:12 +0530

    Update Bipartite test using dfst.cpp

--- Graph/Bipartite test using dfst.cpp
+++ Graph/Bipartite test using dfst.cpp
@@ -1,8 +1,8 @@
 // Each vertex belongs to exactly one of the 2 color(0/1)
 // Each edge connects vertices of 2 different sets
-// If the color of parent and child are same then return false else return true
-
-bool dfs(ll pos,ll ok){
+// If the color of parent and child are same then return true else return false
+   
+bool dfs(int pos,int ok){
 	vis[pos]=1;
 	col[pos]=ok;
 
@@ -10,12 +10,11 @@ bool dfs(ll pos,ll ok){
 		if(!vis[i]){
 			if(dfs(i,1-ok))
 				return true;
-			else return false;
 		}
 		else if(col[pos]==col[i])
-			return false;
+			return true;
 	}
 
-	return true;
+	return false;
 }
 
